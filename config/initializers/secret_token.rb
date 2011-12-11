6cfc392c3ddf09619eb9bb2c644cd3738054e7dd
Aaron Smith <aaron@plotlocker.com>
2011-12-11 16:17:27 -0500

    Initial Commit on Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '4946449b8afdd81b849cc6f7bea334d18de1f3ad24f86750e677bde82ceb74d0bf6b6980c1626a70ef497940d98229052660f479849c6060d8eac055f8b0b813'
